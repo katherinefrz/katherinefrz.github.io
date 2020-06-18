f95447bca4df97f4b4b4dd01c3ef2a86bc0bddb4
Катерина Фуражкина <furazkinakaterina@gmail.com>
2020-06-18 16:43:48 +0300

    добавление проекта

---
+++ 2/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slider-arrow slider-next"><img src="images/next.svg" alt="next arrow"/></button>',
+        prevArrow: '<button class="slider-arrow slider-prev"><img src="images/prev.svg" alt="prev arrow"/></button>'
+    });
+});
+
